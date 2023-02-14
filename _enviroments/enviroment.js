b336bc8c246f1e9095e3c3e60b4e16af7b979b1d
Guillem <saguesguillem@gmail.com>
2023-02-14 11:19:09 +0100

    feat: Footer component

---
+++ _enviroments/enviroment.js
@@ -0,0 +1,3 @@
+export const enviroment = {
+  BASE_API_URL: "http://localhost:3000",
+};
---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Footer.scss";
+
+export function Footer() {
+  return (
+    <div className="footer">
+      <img
+        src="../../../public/img/LOGO MUBA Management.svg"
+        alt="Logo Muba Management & Booking"
+      />
+    </div>
+  );
+}
