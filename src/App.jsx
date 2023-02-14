91385a3ab8f9935cc977100148d53e2b5c2cb4e4
Guillem <saguesguillem@gmail.com>
2023-02-14 16:55:39 +0100

    feat: header & Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -13,6 +13,7 @@ import {
   Navigate,
 } from "react-router-dom";
 import { Footer } from "./components/Footer/Footer";
+import { Header } from "./components/Header/Header";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -20,6 +21,7 @@ function App() {
   return (
     <div className="App">
       <BrowserRouter>
+        <Header />
         <Footer />
       </BrowserRouter>
     </div>
